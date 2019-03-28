e4c87718cc034558d518d6019d48735e8d0d0381
dongmingming <dongmingming@qq.com>
2019-03-28 17:41:00 +0800

    fixed

--- src/apps/application.js
+++ src/apps/application.js
@@ -54,7 +54,7 @@ export const publicMethods = {
             let complete2 = await this.startSignal(actions2.data.types[0]);
             await this.emitMessage(self);
         },
-         hasRequired(data) {
+        hasRequired(data) {
             let $self = this;
             let detailsData = $self.tableData ? $self.tableData : $self.formData;
             let options = [];
@@ -64,22 +64,22 @@ export const publicMethods = {
                     if (detailsData[key]) {
                         options.push(key + "=" + detailsData[key]);
                     } else {
-                        if(key == "oid"){
+                        if (key == "oid") {
                             options.push(key + "=" + this.$store.getters.LoginData.oid);
-                        }else{
-                            if(key.indexOf("filterButton") > -1){
-                            }else{
+                        } else {
+                            if (key.indexOf("filterButton") > -1) {
+                            } else {
                                 $self.msgTips('依赖的' + key + '表单中找不到', "warning");
                                 return false;
                             }
                         }
-                       
+
                     }
                 };
-                if(data.options && data.options.length > 0){
-                    data.options.concat(options); 
-                }else{
-                    if(options){
+                if (data.options && data.options.length > 0) {
+                    data.options.concat(options);
+                } else {
+                    if (options) {
                         data.options = options;
                     }
                 }
@@ -112,9 +112,9 @@ export const publicMethods = {
             }
 
             if ($self.currentAction.action == 'PULL' || $self.currentAction.action == 'REJECT') {
-                $self.currentAction["comment"] =  $self.currentAction.name;
+                $self.currentAction["comment"] = $self.currentAction.name;
                 await $self.startSignal();
-                $self.msgTips($self.currentAction.name +"成功","success");
+                $self.msgTips($self.currentAction.name + "成功", "success");
                 $self.getFormDetailsData();
             } else {
                 $self.dialogVisible = true;
@@ -136,10 +136,10 @@ export const publicMethods = {
                 $self.getFormDetailsData();
             }
             $self.dialogVisible = false;
-            $self.msgTips($self.currentAction.name +"成功","success");
+            $self.msgTips($self.currentAction.name + "成功", "success");
         },
- 
-        
+
+
 
         async getCrumbs() {
             let url = `/workflow/${this.appFlowName}/${
@@ -157,7 +157,7 @@ export const publicMethods = {
             let url = `/workflow/${this.appFlowName}/${
                 this.formId
                 }/getComments`;
-                return await this.$axios.get(url);
+            return await this.$axios.get(url);
         },
         getUsers(url) {
             this.$axios.get(url).then(res => {
@@ -180,92 +180,92 @@ export const publicMethods = {
             });
         },
 
-         //金额阿拉伯数字转大写金额
-         convertCurrency(money) {  
+        //金额阿拉伯数字转大写金额
+        convertCurrency(money) {
             //汉字的数字  
-            var cnNums = new Array('零', '壹', '贰', '叁', '肆', '伍', '陆', '柒', '捌', '玖');  
+            var cnNums = new Array('零', '壹', '贰', '叁', '肆', '伍', '陆', '柒', '捌', '玖');
             //基本单位  
-            var cnIntRadice = new Array('', '拾', '佰', '仟');  
+            var cnIntRadice = new Array('', '拾', '佰', '仟');
             //对应整数部分扩展单位  
-            var cnIntUnits = new Array('', '万', '亿', '兆');  
+            var cnIntUnits = new Array('', '万', '亿', '兆');
             //对应小数部分单位  
-            var cnDecUnits = new Array('角', '分', '毫', '厘');  
+            var cnDecUnits = new Array('角', '分', '毫', '厘');
             //整数金额时后面跟的字符  
-            var cnInteger = '整';  
+            var cnInteger = '整';
             //整型完以后的单位  
-            var cnIntLast = '元';  
+            var cnIntLast = '元';
             //最大处理的数字  
-            var maxNum = 999999999999999.9999;  
+            var maxNum = 999999999999999.9999;
             //金额整数部分  
-            var integerNum;  
+            var integerNum;
             //金额小数部分  
-            var decimalNum;  
+            var decimalNum;
             //输出的中文金额字符串  
-            var chineseStr = '';  
+            var chineseStr = '';
             //分离金额后用的数组，预定义  
-            var parts;  
-            if (money == '') { return ''; }  
-            money = parseFloat(money);  
-            if (money >= maxNum) {  
-              //超出最大处理数字  
-              return '';  
-            }  
-            if (money == 0) {  
-              chineseStr = cnNums[0] + cnIntLast + cnInteger;  
-              return chineseStr;  
-            }  
+            var parts;
+            if (money == '') { return ''; }
+            money = parseFloat(money);
+            if (money >= maxNum) {
+                //超出最大处理数字  
+                return '';
+            }
+            if (money == 0) {
+                chineseStr = cnNums[0] + cnIntLast + cnInteger;
+                return chineseStr;
+            }
             //转换为字符串  
-            money = money.toString();  
-            if (money.indexOf('.') == -1) {  
-              integerNum = money;  
-              decimalNum = '';  
-            } else {  
-              parts = money.split('.');  
-              integerNum = parts[0];  
-              decimalNum = parts[1].substr(0, 4);  
-            }  
+            money = money.toString();
+            if (money.indexOf('.') == -1) {
+                integerNum = money;
+                decimalNum = '';
+            } else {
+                parts = money.split('.');
+                integerNum = parts[0];
+                decimalNum = parts[1].substr(0, 4);
+            }
             //获取整型部分转换  
-            if (parseInt(integerNum, 10) > 0) {  
-              var zeroCount = 0;  
-              var IntLen = integerNum.length;  
-              for (var i = 0; i < IntLen; i++) {  
-                var n = integerNum.substr(i, 1);  
-                var p = IntLen - i - 1;  
-                var q = p / 4;  
-                var m = p % 4;  
-                if (n == '0') {  
-                  zeroCount++;  
-                } else {  
-                  if (zeroCount > 0) {  
-                    chineseStr += cnNums[0];  
-                  }  
-                  //归零  
-                  zeroCount = 0;  
-                  chineseStr += cnNums[parseInt(n)] + cnIntRadice[m];  
-                }  
-                if (m == 0 && zeroCount < 4) {  
-                  chineseStr += cnIntUnits[q];  
-                }  
-              }  
-              chineseStr += cnIntLast;  
-            }  
+            if (parseInt(integerNum, 10) > 0) {
+                var zeroCount = 0;
+                var IntLen = integerNum.length;
+                for (var i = 0; i < IntLen; i++) {
+                    var n = integerNum.substr(i, 1);
+                    var p = IntLen - i - 1;
+                    var q = p / 4;
+                    var m = p % 4;
+                    if (n == '0') {
+                        zeroCount++;
+                    } else {
+                        if (zeroCount > 0) {
+                            chineseStr += cnNums[0];
+                        }
+                        //归零  
+                        zeroCount = 0;
+                        chineseStr += cnNums[parseInt(n)] + cnIntRadice[m];
+                    }
+                    if (m == 0 && zeroCount < 4) {
+                        chineseStr += cnIntUnits[q];
+                    }
+                }
+                chineseStr += cnIntLast;
+            }
             //小数部分  
-            if (decimalNum != '') {  
-              var decLen = decimalNum.length;  
-              for (var i = 0; i < decLen; i++) {  
-                var n = decimalNum.substr(i, 1);  
-                if (n != '0') {  
-                  chineseStr += cnNums[Number(n)] + cnDecUnits[i];  
-                }  
-              }  
-            }  
-            if (chineseStr == '') {  
-              chineseStr += cnNums[0] + cnIntLast + cnInteger;  
-            } else if (decimalNum == '') {  
-              chineseStr += cnInteger;  
-            }  
-            return chineseStr;  
-          } 
+            if (decimalNum != '') {
+                var decLen = decimalNum.length;
+                for (var i = 0; i < decLen; i++) {
+                    var n = decimalNum.substr(i, 1);
+                    if (n != '0') {
+                        chineseStr += cnNums[Number(n)] + cnDecUnits[i];
+                    }
+                }
+            }
+            if (chineseStr == '') {
+                chineseStr += cnNums[0] + cnIntLast + cnInteger;
+            } else if (decimalNum == '') {
+                chineseStr += cnInteger;
+            }
+            return chineseStr;
+        }
     },
 
 };
