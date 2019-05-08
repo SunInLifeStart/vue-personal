390a1ecdf730425a2ee60e904d6a6062e1ab6316
panjianzhuang <1178221581@qq.com>
2019-05-08 11:39:54 +0800

    部门人员bug

--- src/apps/discussion/DiscussionDetail.vue
+++ src/apps/discussion/DiscussionDetail.vue
@@ -241,7 +241,7 @@ export default {
         },
         searchPersonOptions(array, value) {
             array.forEach(item => {
-                if (item.id === value) {
+                if (item.id === value && item.type) {
                     this.person = item.children;
                 }
                 if (item.children && item.children.length > 0) {
