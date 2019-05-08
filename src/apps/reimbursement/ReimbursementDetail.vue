5a26b5255cd36b0843407e52d5c5c4fa383b3d8e
zhouchenyang <1648060322@qq.com>
2019-05-08 17:14:17 +0800

    提交了报销审批单

--- src/apps/reimbursement/ReimbursementDetail.vue
+++ src/apps/reimbursement/ReimbursementDetail.vue
@@ -16,12 +16,12 @@
         <div class="formContent" style="margin-top: 5px;margin-left:10px;overflow:auto;">
             <div>
                 <el-button type="primary" @click="getFlowNode" v-show="this.tableData.status && this.tableData.status != '04'">查看流程</el-button>
-                <el-button style="margin-left: 25px;" type="primary" @click="print">打印</el-button>
+                <el-button style="margin-left: 25px;" type="primary" @click="print" v-show="this.tableData.status && this.tableData.status == '04'">打印</el-button>
             </div>
             <br />
             <el-form :model='tableData' class="demo-form-inline" ref="formupdate">
                 <h4 style="text-align: center;">报销审批单</h4>
-                <el-row style="margin-top: 20px;">
+                <el-row style="margin-top: 10px;">
                     <el-col :span="7">
                         <el-form-item label="单据编号：">
                             <span style="font-size:10px">
@@ -40,7 +40,7 @@
                         </el-form-item>
                     </el-col>
                 </el-row>
-                <table class="tablePrint" style="width: 99%; height: 100%; table-layout: fixed; word-break: break-all;margin-top:10px;">
+                <table class="tablePrint" style="width: 99%; height: 100%; table-layout: fixed; word-break: break-all;margin-top:5px;">
                     <col style="width: 10%" />
                     <col style="width: 10%" />
                     <col style="width: 10%" />
@@ -345,23 +345,24 @@
                         </td>
                     </tr>
                 </table>
-            </el-form>
-            <el-row v-if="comments && comments.length > 0">
-                <el-col :span="24">
-                    <h3>审批意见</h3>
-                    <div class="items">
-                        <div class="item" v-for="item in comments" :key="item.id">
-                            <div class="avatar"><img src="img/avatar.1176c00a.png" alt="" width="30px"></div>
-                            <div class="info">
-                                <div class="creator">
-                                    <span href="#">{{item.userName}}</span> &nbsp; ({{item.times | dateformat}})
+                <el-row v-if="comments && comments.length > 0">
+                    <el-col :span="24">
+                        <h3>审批意见</h3>
+                        <div class="items">
+                            <div class="item" v-for="item in comments" :key="item.id">
+                                <div class="avatar"><img src="img/avatar.1176c00a.png" alt="" width="30px"></div>
+                                <div class="info">
+                                    <div class="creator">
+                                        <span href="#">{{item.userName}}</span> &nbsp; ({{item.times | dateformat}})
+                                    </div>
+                                    <div class="content">{{item.fullMessage}}</div>
                                 </div>
-                                <div class="content">{{item.fullMessage}}</div>
                             </div>
                         </div>
-                    </div>
-                </el-col>
-            </el-row>
+                    </el-col>
+                </el-row>
+            </el-form>
+
         </div>
         <el-dialog :visible.sync="dialogVisible" center width="30%" append-to-body>
             <el-form>
