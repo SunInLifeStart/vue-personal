d4e587843303e6a6b5cc2e68f2555d65b5ad4be9
chenqin <13031117181@163.com>
2019-05-13 10:07:55 +0800

    cq

--- src/apps/files/index.vue
+++ src/apps/files/index.vue
@@ -76,10 +76,12 @@
                     </el-table-column>
                     <el-table-column label="操作">
                         <template slot-scope="scope">
-                            <el-tooltip class="item" effect="dark" content="编辑" placement="left">
+                            <el-tooltip class="item" effect="dark" content="编辑" placement="left"
+                            v-if="scope.row.status == '00' || scope.row.status == '02'">
                                 <el-button type="text" icon="el-icon-edit-outline" @click="editForm(scope.row)" v-show="scope.row.status!='01'&&scope.row.status!='04'"></el-button>
                             </el-tooltip>
-                            <el-tooltip class="item" effect="dark" content="删除" placement="right">
+                            <el-tooltip class="item" effect="dark" content="删除" placement="right"
+                            v-if="scope.row.status == '00' || scope.row.status == '02'">
                                 <el-button type="text" icon="el-icon-delete" @click.stop="deleteCurrentLine(scope.row.id)" v-show="scope.row.status!='01'&&scope.row.status!='04'"></el-button>
                             </el-tooltip>
                         </template>
