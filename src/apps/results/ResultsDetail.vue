4513f6bd155791247ca742528cffa9216ee603e6
panjianzhuang <1178221581@qq.com>
2019-05-20 10:17:13 +0800

    采购结果bug修改

--- src/apps/results/ResultsDetail.vue
+++ src/apps/results/ResultsDetail.vue
@@ -20,11 +20,11 @@
                         </el-form-item>
                     </el-col>
                     <el-col :span="8">
-                        <el-form-item label="项目名称：">{{tableData.purchaseProjectName}}
+                        <el-form-item label="项目名称：">{{tableData.projectName}}
                         </el-form-item>
                     </el-col>
                     <el-col :span="8">
-                        <el-form-item label="采购项目名称：">{{tableData.organName}}
+                        <el-form-item label="采购项目名称：">{{tableData.purchaseProjectName}}
                         </el-form-item>
                     </el-col>
                 </el-row>
