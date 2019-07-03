028a7b9184b92115d563308e4c43e4ebdd8b1570
zhouchenyang <1648060322@qq.com>
2019-07-03 17:11:44 +0800

    提交了发文

--- src/apps/outgoing/index.vue
+++ src/apps/outgoing/index.vue
@@ -1,45 +1,42 @@
 <template>
     <div id="Outgoing">
         <el-card class="box-card">
-               <!-- 查询 -->
-                <div id="OutgoingFilter">
-                    <el-form :inline="true"  class="demo-form-inline">
-                        <el-row class="filterForm">
-                            <el-col :span="8">
-                                <el-form-item label="标题" label-width="50px">
-                                    <el-input v-model="params.title" placeholder="请输入标题"></el-input>
-                                </el-form-item>
+            <!-- 查询 -->
+            <div id="OutgoingFilter">
+                <el-form :inline="true" class="demo-form-inline">
+                    <el-row class="filterForm">
+                        <el-col :span="8">
+                            <el-form-item label="标题" label-width="50px">
+                                <el-input v-model="params.title" placeholder="请输入标题"></el-input>
+                            </el-form-item>
 
-                            </el-col>
-                            <el-col :span="8">
-                                <el-form-item label="单据状态">
-                                    <el-select v-model="params.status" clearable placeholder="请选择">
-                                        <el-option v-for="item in s_status" 
-                                        :key="item.value"
-                                        :label="item.label"
-                                        :value="item.value">
-                                        </el-option>
-                                    </el-select>
-                                </el-form-item>
-                               
-                            </el-col>
-                            <el-col :span="8" class="searchBtn">
-                                <el-form-item class="positionBtn">
-                                    <el-button type="primary" @click="searchList">查询</el-button>
-                                    <el-button  @click="resetInput">重置</el-button>
-                                </el-form-item>
-                            </el-col>
+                        </el-col>
+                        <el-col :span="8">
+                            <el-form-item label="单据状态">
+                                <el-select v-model="params.status" clearable placeholder="请选择">
+                                    <el-option v-for="item in s_status" :key="item.value" :label="item.label" :value="item.value">
+                                    </el-option>
+                                </el-select>
+                            </el-form-item>
+
+                        </el-col>
+                        <el-col :span="8" class="searchBtn">
+                            <el-form-item class="positionBtn">
+                                <el-button type="primary" @click="searchList">查询</el-button>
+                                <el-button @click="resetInput">重置</el-button>
+                            </el-form-item>
+                        </el-col>
                     </el-row>
-                    </el-form>
-                </div>
+                </el-form>
+            </div>
 
-                  <!-- 新建 -->
-                <div class="toolbar">
-                    <el-button type="primary" icon="el-icon-plus" @click="createNewForm">新建</el-button>
-                </div>
-                <div id="OutgoingList">
-               
-                 <el-table :data="tableData" stripe style="margin-bottom: 10px;" @row-click="showCurrentId" highlight-current-row>
+            <!-- 新建 -->
+            <div class="toolbar">
+                <el-button type="primary" icon="el-icon-plus" @click="createNewForm">新建</el-button>
+            </div>
+            <div id="OutgoingList">
+
+                <el-table :data="tableData" stripe style="margin-bottom: 10px;" @row-click="showCurrentId" highlight-current-row>
                     <el-table-column prop="title" label="标题"></el-table-column>
                     <el-table-column prop="creatorName" label="拟稿人" width="200"></el-table-column>
                     <el-table-column prop="organName" label="拟稿单位" width="200"></el-table-column>
@@ -56,80 +53,77 @@
                     <el-table-column label="操作" width="150" align="center">
                         <template slot-scope="scope">
                             <!-- v-if="scope.row.status == '已驳回' || scope.row.status == '已保存'" -->
-                            <el-tooltip class="item" effect="dark" content="编辑" placement="left" 
-                             v-if="scope.row.status == '00' || scope.row.status == '02'">
+                            <el-tooltip class="item" effect="dark" content="编辑" placement="left" v-if="scope.row.status == '00' || scope.row.status == '02'">
                                 <el-button type="text" icon="el-icon-edit-outline" @click="editForm(scope.row)"></el-button>
                             </el-tooltip>
-                            <el-tooltip class="item" effect="dark" content="删除" placement="left"
-                             v-if="scope.row.status == '00' || scope.row.status == '02'">
+                            <el-tooltip class="item" effect="dark" content="删除" placement="left" v-if="scope.row.status == '00'">
                                 <el-button type="text" icon="el-icon-delete" @click="deleteForm(scope.row)"></el-button>
                             </el-tooltip>
                         </template>
                     </el-table-column>
                 </el-table>
-                   <br />
-                 <el-pagination @size-change="sizeChange" @current-change="currentChange" :current-page="params.page" :page-sizes="[5, 10, 30, 50]" :page-size="params.pageSize" layout="total, sizes, prev, pager, next, jumper" :total="params.total"></el-pagination>
+                <br />
+                <el-pagination @size-change="sizeChange" @current-change="currentChange" :current-page="params.page" :page-sizes="[5, 10, 30, 50]" :page-size="params.pageSize" layout="total, sizes, prev, pager, next, jumper" :total="params.total"></el-pagination>
             </div>
-          </el-card>
+        </el-card>
         <br>
         <el-card class="box-card">
-            <OutgoingDetail :formId="formId" ref="OutgoingDetail" @reloadList = "reloadList" @resetStatus = "resetStatus"></OutgoingDetail>
-             <!-- :formId="formId" -->
+            <OutgoingDetail :formId="formId" ref="OutgoingDetail" @reloadList="reloadList" @resetStatus="resetStatus"></OutgoingDetail>
+            <!-- :formId="formId" -->
         </el-card>
-         <OutgoingForm  ref="OutgoingForm" @reloadList = "reloadList"></OutgoingForm>
-         <!-- :formDataFromIndex="formDataFromIndex"  -->
+        <OutgoingForm ref="OutgoingForm" @reloadList="reloadList"></OutgoingForm>
+        <!-- :formDataFromIndex="formDataFromIndex"  -->
     </div>
 </template>
 <script>
-import axios from "axios";
-import OutgoingForm from "./OutgoingForm";
-import OutgoingDetail from "./OutgoingDetail";
-import {publicMethods} from "../application.js";
+import axios from 'axios';
+import OutgoingForm from './OutgoingForm';
+import OutgoingDetail from './OutgoingDetail';
+import { publicMethods } from '../application.js';
 export default {
-    mixins:[publicMethods],
-    name: "Outgoing",
+    mixins: [publicMethods],
+    name: 'Outgoing',
     data() {
         return {
             tableData: [],
             formDetails: {},
-            formId: "",
+            formId: '',
             s_status: [
-                    {
-                        value: '00',
-                        label: '已保存'
-                    },
-                    {
-                        value: '01',
-                        label: '审核中'
-                    },
-                    {
-                        value: '02',
-                        label: '已驳回'
-                    },
-                   
-                    {
-                        value: '04',
-                        label: '已完成'
-                    }
-                ],
+                {
+                    value: '00',
+                    label: '已保存'
+                },
+                {
+                    value: '01',
+                    label: '审核中'
+                },
+                {
+                    value: '02',
+                    label: '已驳回'
+                },
+
+                {
+                    value: '04',
+                    label: '已完成'
+                }
+            ],
             params: {
                 page: 1,
                 pageSize: 5,
-                department: "",
-                title: "",
+                department: '',
+                title: '',
                 total: 0,
-                committed:"",
-                status:"",
+                committed: '',
+                status: '',
                 // outgoingingTime:[],
                 // startTime:"",
                 // endTime:"",
-                orderBy: "created",
+                orderBy: 'created',
                 desc: true,
                 options: []
-               
             },
             searchOptions: [],
-            formName:"outgoingingApplication"
+            formName: 'outgoingingApplication'
         };
     },
     components: {
@@ -139,10 +133,10 @@ export default {
     filters: {
         filterStatus: function(data) {
             let xmlJson = {
-               "00":"已保存", 
-               "01":"审核中",
-               "02" :"已驳回",
-               "04" :"已完成"
+                '00': '已保存',
+                '01': '审核中',
+                '02': '已驳回',
+                '04': '已完成'
             };
             return xmlJson[data];
         }
@@ -168,22 +162,21 @@ export default {
                     });
             });
         },
-        
+
         //获取列表
-         async getList(page) {
+        async getList(page) {
             let $self = this;
-            $self.url = "/api/v1/outgoing_forms/query";
+            $self.url = '/api/v1/outgoing_forms/query';
             let response = await $self.getQueryList();
             if (response) {
                 if (response.data.forms && response.data.forms.length > 0) {
-                   let formId = response.data.forms[0].id;
-                   $self.$refs.OutgoingDetail.getFormDetails(formId);
+                    let formId = response.data.forms[0].id;
+                    $self.$refs.OutgoingDetail.getFormDetails(formId);
                 }
-               $self.tableData = response.data.forms;
+                $self.tableData = response.data.forms;
                 $self.params.total = response.data.totalCount;
-                
             } else {
-               // $self.msgTips("获取列表失败", "warning");
+                // $self.msgTips("获取列表失败", "warning");
             }
         },
 
@@ -194,8 +187,7 @@ export default {
 
         //新建
         createNewForm() {
-             this.$refs.OutgoingForm.createForm();
-            
+            this.$refs.OutgoingForm.createForm();
         },
 
         //编辑
@@ -203,18 +195,18 @@ export default {
             this.$refs.OutgoingForm.setDataFromParent(data);
         },
         reloadList(params) {
-            if (params == "reload") {
+            if (params == 'reload') {
                 this.params.page = 1;
                 this.getList();
             } else {
                 this.$refs.OutgoingDetail.getFormDetails(params.id);
             }
         },
-        resetStatus(data){
-              let $self = this;
-            for(let item of $self.tableData){
-                if(data.id == item.id){
-                  item.status = data.status;
+        resetStatus(data) {
+            let $self = this;
+            for (let item of $self.tableData) {
+                if (data.id == item.id) {
+                    item.status = data.status;
                 }
             }
         },
@@ -232,9 +224,9 @@ export default {
             this.searchOptions = [];
             if (this.params.title.trim() !== '') {
                 this.searchOptions.push({
-                    'field': 'title',
-                    'filter': 'LIKE',
-                    'value': this.params.title
+                    field: 'title',
+                    filter: 'LIKE',
+                    value: this.params.title
                 });
             }
             if (this.params.status.trim() !== '') {
@@ -244,26 +236,25 @@ export default {
                     value: this.params.status
                 });
             }
-            this.params.options=this.searchOptions
+            this.params.options = this.searchOptions;
             this.getList();
         },
         resetInput() {
-           this.params={
+            this.params = {
                 page: 1,
                 pageSize: 5,
-                department: "",
-                title: "",
+                department: '',
+                title: '',
                 total: 0,
-                committed:"",
-                status:"",
+                committed: '',
+                status: '',
                 // outgoingingTime:[],
                 // startTime:"",
                 // endTime:"",
-                orderBy: "created",
+                orderBy: 'created',
                 desc: true,
                 options: []
-               
-            }
+            };
             this.getList();
             // this.s_status=[]
         }
@@ -274,27 +265,27 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
-      #OutgoingFilter  .el-form-item--small.el-form-item{
-            width: 100%;
+#OutgoingFilter .el-form-item--small.el-form-item {
+    width: 100%;
+}
+#OutgoingFilter {
+    .searchBtn {
+        padding-right: 10px;
+        .positionBtn {
+            text-align: right;
         }
-         #OutgoingFilter {
-             .searchBtn {
-            padding-right: 10px;
-            .positionBtn{
-                text-align: right;
-            }
-        }}
+    }
+}
 </style>
 <style scoped>
-
-#OutgoingFilter .filterForm >>> .el-form-item__content{
-        width: calc(100% - 80px);
-    }
-    #OutgoingFilter .filterForm >>> .el-select {
-        width: calc(100% - 15px);
-    }
-    #OutgoingFilter .filterForm >>> .el-date-editor{
-        width: calc(100% - 0px);
-    }
+#OutgoingFilter .filterForm >>> .el-form-item__content {
+    width: calc(100% - 80px);
+}
+#OutgoingFilter .filterForm >>> .el-select {
+    width: calc(100% - 15px);
+}
+#OutgoingFilter .filterForm >>> .el-date-editor {
+    width: calc(100% - 0px);
+}
 </style>
 
