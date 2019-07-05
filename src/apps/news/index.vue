a5a1901e43276c0f4e03ec33c56a7f110021d9e9
zhouchenyang <1648060322@qq.com>
2019-07-05 09:36:55 +0800

    提交了新闻查询条件

--- src/apps/news/index.vue
+++ src/apps/news/index.vue
@@ -1,8 +1,8 @@
 <template>
     <div id="News">
         <el-card class="box-card">
-               <!-- 查询 -->
-            <el-form :inline="true" label-width="70px"  label-position="left" :model="params" class="demo-form-inline">
+            <!-- 查询 -->
+            <el-form :inline="true" label-width="70px" label-position="left" :model="params" class="demo-form-inline">
                 <el-row class="filterForm">
                     <el-col :span="8">
                         <el-form-item label="标题">
@@ -22,13 +22,14 @@
                 </el-row>
                 <el-row class="filterForm">
                     <el-col :span="8">
+                        <el-form-item label="公司部门">
+                            <el-input v-model="params.organName" placeholder="公司部门"></el-input>
+                        </el-form-item>
+                    </el-col>
+                    <el-col :span="8">
                         <el-form-item label="状态">
                             <el-select v-model="params.status" placeholder="请选择">
-                                <el-option
-                                        v-for="item in statusOption"
-                                        :key="item.value"
-                                        :label="item.label"
-                                        :value="item.value">
+                                <el-option v-for="item in statusOption" :key="item.value" :label="item.label" :value="item.value">
                                 </el-option>
                             </el-select>
                         </el-form-item>
@@ -38,6 +39,9 @@
                             <el-date-picker v-model="params.created" type="daterange" range-separator="至" start-placeholder="开始日期" end-placeholder="结束日期"></el-date-picker>
                         </el-form-item>
                     </el-col>
+
+                </el-row>
+                <el-row class="filterForm">
                     <el-col :span="8">
                         <el-form-item>
                             <el-button type="primary" @click="searchList">查询</el-button>
@@ -47,11 +51,11 @@
                 </el-row>
             </el-form>
 
-                  <!-- 新建 -->
-                <div class="toolbar">
-                    <el-button type="primary" icon="el-icon-plus" @click="createNewForm">新建</el-button>
-                </div>
-                <div id="NewsList">
+            <!-- 新建 -->
+            <div class="toolbar">
+                <el-button type="primary" icon="el-icon-plus" @click="createNewForm">新建</el-button>
+            </div>
+            <div id="NewsList">
                 <el-table :data="tableData" stripe style="width: 100%; cursor:pointer" @row-click="showCurrentId" highlight-current-row>
                     <el-table-column prop="title" label="标题">
                     </el-table-column>
@@ -62,47 +66,45 @@
                     <el-table-column prop="creatorName" label="作者">
                     </el-table-column>
                     <el-table-column prop="created" width="250" label="创建时间"></el-table-column>
-                     <el-table-column  width="100" label="单据状态">
-                         <template slot-scope="scope">{{scope.row.status | filterStatus}}</template>
-                     </el-table-column>
+                    <el-table-column width="100" label="单据状态">
+                        <template slot-scope="scope">{{scope.row.status | filterStatus}}</template>
+                    </el-table-column>
                     <el-table-column label="操作" width="100">
-                    <template slot-scope="scope">
-                        <el-tooltip class="item" effect="dark" content="编辑" placement="left"
-                            v-if="scope.row.status == '00' || scope.row.status == '02'" >
-                            <el-button type="text" icon="el-icon-edit-outline" @click="editForm(scope.row)"></el-button>
-                        </el-tooltip>
-                        <el-tooltip class="item" effect="dark" content="删除" placement="left"
-                            v-if="scope.row.status == '00'">
-                            <el-button type="text" icon="el-icon-delete" @click.stop="deleteCurrentLine(scope.row.id)"></el-button>
-                        </el-tooltip>
-                    </template>
-                </el-table-column>
+                        <template slot-scope="scope">
+                            <el-tooltip class="item" effect="dark" content="编辑" placement="left" v-if="scope.row.status == '00' || scope.row.status == '02'">
+                                <el-button type="text" icon="el-icon-edit-outline" @click="editForm(scope.row)"></el-button>
+                            </el-tooltip>
+                            <el-tooltip class="item" effect="dark" content="删除" placement="left" v-if="scope.row.status == '00'">
+                                <el-button type="text" icon="el-icon-delete" @click.stop="deleteCurrentLine(scope.row.id)"></el-button>
+                            </el-tooltip>
+                        </template>
+                    </el-table-column>
                 </el-table>
-                   <br />
-                 <el-pagination @size-change="sizeChange" @current-change="currentChange" :current-page="params.page" :page-sizes="[5, 10, 30, 50]" :page-size="params.pageSize" layout="total, sizes, prev, pager, next, jumper" :total="params.total"></el-pagination>
+                <br />
+                <el-pagination @size-change="sizeChange" @current-change="currentChange" :current-page="params.page" :page-sizes="[5, 10, 30, 50]" :page-size="params.pageSize" layout="total, sizes, prev, pager, next, jumper" :total="params.total"></el-pagination>
             </div>
-          </el-card>
+        </el-card>
         <br>
         <el-card class="box-card">
-            <newsDetail :formId="formId" ref="newsDetail" @reloadList = "reloadList" @resetStatus = "resetStatus"></newsDetail>
+            <newsDetail :formId="formId" ref="newsDetail" @reloadList="reloadList" @resetStatus="resetStatus"></newsDetail>
         </el-card>
-         <newsForm  ref="newsForm" @reloadList = "reloadList"></newsForm>
+        <newsForm ref="newsForm" @reloadList="reloadList"></newsForm>
     </div>
 </template>
 <script>
-import newsForm from "./NewsForm";
-import moment from "moment";
-import newsDetail from "./NewsDetail";
-import {publicMethods} from "../application.js";
+import newsForm from './NewsForm';
+import moment from 'moment';
+import newsDetail from './NewsDetail';
+import { publicMethods } from '../application.js';
 export default {
-    mixins:[publicMethods],
-    name: "news",
+    mixins: [publicMethods],
+    name: 'news',
     data() {
         return {
             tableData: [],
             searchOptions: [],
             formDetails: {},
-            formId: "",
+            formId: '',
             statusOption: [
                 {
                     value: '00',
@@ -126,9 +128,10 @@ export default {
                 pageSize: 5,
                 desc: true,
                 options: [],
-                orderBy: "created"
+                orderBy: 'created',
+                organName: ''
             },
-            formName:"news_forms"
+            formName: 'news_forms'
         };
     },
     components: {
@@ -138,25 +141,25 @@ export default {
     filters: {
         filterStatus: function(data) {
             let xmlJson = {
-               "00":"已保存", 
-               "01":"审核中",
-               "02" :"已驳回",
-               "03" :"已撤销",
-               "04" :"已完成"
+                '00': '已保存',
+                '01': '审核中',
+                '02': '已驳回',
+                '03': '已撤销',
+                '04': '已完成'
             };
             return xmlJson[data];
         }
     },
     methods: {
         //获取列表
-         async getList(pageNum) {
+        async getList(pageNum) {
             let $self = this;
-            $self.url = "/api/v1/news_forms/query";
+            $self.url = '/api/v1/news_forms/query';
             let response = await $self.getQueryList();
             if (response) {
                 if (response.data.forms.length > 0) {
-                   let formId = response.data.forms[0].id;
-                   $self.$refs.newsDetail.getFormDetails(formId);
+                    let formId = response.data.forms[0].id;
+                    $self.$refs.newsDetail.getFormDetails(formId);
                 }
                 $self.tableData = response.data.forms;
                 $self.params.total = response.data.totalCount;
@@ -178,18 +181,18 @@ export default {
             this.$refs.newsForm.setDataFromParent(data);
         },
         reloadList(params) {
-            if (params == "reload") {
+            if (params == 'reload') {
                 this.params.page = 1;
                 this.getList();
             } else {
                 this.$refs.newsDetail.getFormDetails(params.id);
             }
         },
-        resetStatus(data){
-              let $self = this;
-            for(let item of $self.tableData){
-                if(data.id == item.id){
-                  item.status = data.status;
+        resetStatus(data) {
+            let $self = this;
+            for (let item of $self.tableData) {
+                if (data.id == item.id) {
+                    item.status = data.status;
                 }
             }
         },
@@ -212,30 +215,39 @@ export default {
                     value: this.params.title
                 });
             }
-            if (this.params.creatorName && this.params.creatorName.trim() !== '') {
+            if (
+                this.params.creatorName &&
+                this.params.creatorName.trim() !== ''
+            ) {
                 this.searchOptions.push({
                     field: 'creatorName',
                     filter: 'EQUAL',
                     value: this.params.creatorName
                 });
             }
-            if (this.params.reportingOrg && this.params.reportingOrg.trim() !== '') {
+            if (
+                this.params.reportingOrg &&
+                this.params.reportingOrg.trim() !== ''
+            ) {
                 this.searchOptions.push({
                     field: 'reportingOrg',
                     filter: 'LIKE',
                     value: this.params.reportingOrg
                 });
             }
+            if (this.params.organName && this.params.organName.trim() !== '') {
+                this.searchOptions.push({
+                    field: 'organName',
+                    filter: 'LIKE',
+                    value: this.params.organName
+                });
+            }
             if (this.params.created && this.params.created.length > 0) {
                 this.searchOptions.push({
                     field: 'created',
                     filter: 'BETWEEN',
-                    value: moment(this.params.created[0]).format(
-                        'YYYY-MM-DD'
-                    ),
-                    value2: moment(this.params.created[1]).format(
-                        'YYYY-MM-DD'
-                    )
+                    value: moment(this.params.created[0]).format('YYYY-MM-DD'),
+                    value2: moment(this.params.created[1]).format('YYYY-MM-DD')
                 });
             }
             if (this.params.status && this.params.status.trim() !== '') {
@@ -245,17 +257,19 @@ export default {
                     value: this.params.status
                 });
             }
-            this.params.options = this.searchOptions
+            this.params.options = this.searchOptions;
             this.getList();
         },
         resetInput() {
-            this.params={
+            this.params = {
                 page: 1,
                 pageSize: 5,
                 desc: true,
                 options: [],
-                orderBy: "created"
-            }
+                orderBy: 'created',
+                organName: ''
+            };
+            this.getList();
         }
     },
     mounted() {
@@ -264,19 +278,19 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
-      #News  .el-form-item--small.el-form-item{
-            width: 100%;
-        }
+#News .el-form-item--small.el-form-item {
+    width: 100%;
+}
 </style>
 <style scoped>
-    #News .filterForm >>> .el-form-item__content{
-        width: calc(100% - 80px);
-    }
-    #News .filterForm >>> .el-select {
-        width: 100%;
-    }
-    #News .filterForm >>> .el-date-editor{
-        width: calc(100% - 0px);
-    }
+#News .filterForm >>> .el-form-item__content {
+    width: calc(100% - 80px);
+}
+#News .filterForm >>> .el-select {
+    width: 100%;
+}
+#News .filterForm >>> .el-date-editor {
+    width: calc(100% - 0px);
+}
 </style>
 
